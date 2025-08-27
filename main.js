8fc41aa9ff0ec6fd596069fbdf43e98ef245f87a
Obbacon <rayx70@live.ca>
2025-08-26 22:54:55 -0400

    new

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const apiKey = "5b89ef6615d4c11a203d0556c88edad9";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -6,17 +7,19 @@ const searchBtn = document.querySelector(".search button");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
+    var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
 }
 
 searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
+    checkWeather(searchBox.value)
 })
+
+
